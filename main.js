fe8c8774ca7c07a66ae8670312cddde76dd9fc95
Marco Tuzza <marcotuzza@hotmail.it>
2020-05-25 17:43:09 +0200

    Chiamata serie tv aggiunta, manca ottimizzazione

--- main.js
+++ main.js
@@ -34,12 +34,33 @@ function find() {
 
                 var star = Math.ceil(filmList[i].vote_average / 2)
 
-                console.log(star);
-
                 var context = {
+
                     "title" : filmList[i].title,
+
                     "titleOriginal" : filmList[i].original_title,
-                    "language" : filmList[i].original_language,
+
+                    "vote" : "voteFilm",
+
+                    "language" : function() {
+
+                        var languages = ["it" , "fr", "en", "de"]
+
+                        if (languages.includes(filmList[i].original_language)) {
+
+                            return "<img src='flags/" + filmList[i].original_language + ".png'>"
+
+                            console.log("Ok");
+
+                        } else {
+
+                            return "<span>" + filmList[i].original_language + "</span>"
+
+                            console.log("No");
+
+                        }
+
+                    }
                 };
 
                 var html = template(context);
@@ -48,9 +69,91 @@ function find() {
 
                 for (var x = 0; x < star; x++) {
 
-                    $(".vote").eq(i).append("<i class='fas fa-star'></i>")
+                    $(".voteFilm").eq(i).append("<i class='fas fa-star'></i>")
 
                 };
+
+                for (var x = 0; x < 5-star; x++) {
+
+                    $(".voteFilm").eq(i).append("<i class='far fa-star'></i>")
+
+                };
+
+            };
+        },
+
+        "error" : console.log("errore")
+    })
+
+    $.ajax({
+        
+        "url" : "https://api.themoviedb.org/3/search/tv",
+
+        "method" : "get",
+
+        "data" : {
+
+            "api_key" : "6ccedbd7f57e862715c5eac090a719e7",
+
+            "query" : value,
+
+            "language" : "it"
+
+        },
+
+        "success" : function(answer) {
+
+            var tvList = answer.results;
+
+            for (var i = 0; i < tvList.length; i++) {
+
+                var star = Math.ceil(tvList[i].vote_average / 2)
+
+                var context = {
+
+                    "title" : tvList[i].name,
+
+                    "titleOriginal" : tvList[i].original_name,
+
+                    "vote" : "voteTv",
+
+                    "language" : function() {
+
+                        var languages = ["it" , "fr", "en", "de"]
+
+                        if (languages.includes(tvList[i].original_language)) {
+
+                            return "<img src='flags/" + tvList[i].original_language + ".png'>"
+
+                            console.log("Ok");
+
+                        } else {
+
+                            return "<span>" + tvList[i].original_language + "</span>"
+
+                            console.log("No");
+
+                        }
+
+                    }
+                };
+
+                var html = template(context);
+
+                $("main").append(html);
+
+                for (var x = 0; x < star; x++) {
+
+                    $(".voteTv").eq(i).append("<i class='fas fa-star'></i>")
+
+                };
+
+                for (var x = 0; x < 5-star; x++) {
+
+                    $(".voteTv").eq(i).append("<i class='far fa-star'></i>")
+
+                };
+
             };
         },
 
